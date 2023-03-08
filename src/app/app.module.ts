f0993ef87be0719777523387503dd5979eba352b
jpatel7312 <jaypatel7312@gmail.com>
2023-03-07 21:00:19 -0500

    Completed Assignment-6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentTypeFilterPipe } from './content-type-filter.pipe';
 import { HoverAffectDirective } from './hover-affect.directive';
+import { AppMessagesComponent } from './app-messages/app-messages.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { HoverAffectDirective } from './hover-affect.directive';
     ContentCardComponent,
     ContentListComponent,
     ContentTypeFilterPipe,
-    HoverAffectDirective
+    HoverAffectDirective,
+    AppMessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -2,6 +2,8 @@ import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Content } from '../../../src/helper-files/content-interface';
+import { EntertainmentServiceService } from '../entertainment-service.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-content-list',
@@ -10,84 +12,45 @@ import { Content } from '../../../src/helper-files/content-interface';
 })
 export class ContentListComponent {
   contentListArr:Content[] = [];
+  retrievedContent:Content|any;
   displayMsgCode:number=-2;
-  generateContent(id:number,contentArr:(string)[]){
-    return {
-      id,
-      title:contentArr[0],
-      description:contentArr[1],
-      creator:contentArr[2]
-    }
-  }
 
   userTitleInputForm = this.formBuilder.group({
-    contentTitleField: ''
+    contentTitleField: 0
   });
 
-  onSubmit=()=>{
-    if (!this.userTitleInputForm.controls.contentTitleField.value) {
-      return;
-    }
-    this.displayMsgCode = this.contentListArr.findIndex(content=>content.title.toLowerCase()===this.userTitleInputForm.controls.contentTitleField.value?.toLowerCase());
-    if(this.displayMsgCode>-1){
-      this.contentListArr[this.displayMsgCode].highlight = true;   
-      this.scroller.scrollToAnchor(this.contentListArr[this.displayMsgCode].title);   
-    }
-    setTimeout(() => {
-      this.contentListArr[this.displayMsgCode].highlight = false;
-      this.displayMsgCode = -2;
-    }, 5000);
+  onSubmit=(id?:number)=>{
+    this.entrnService.getContentAtId(id?id:this.userTitleInputForm.controls.contentTitleField.value).subscribe((content)=>{
+      this.msgService.clear();
+      if (content) {
+        this.retrievedContent={...content};
+        this.retrievedContent.highlight = true;
+        this.msgService.add({status:1,msg:`Content Item at id: ${id?id:this.userTitleInputForm.controls.contentTitleField.value}`});
+      } else{
+        this.retrievedContent='';
+        this.msgService.add({status:0,msg:'There was an error getting content!!'});
+      }
+
+    })
+
   }
 
-  constructor(private formBuilder: FormBuilder, private scroller: ViewportScroller){
-    this.contentListArr.push({
-      ...this.generateContent(
-        1,['Avatar','Most popular aminated movie of all time','James Cameron']
-      ),
-      ...{imgURL:'../assets/avatar.jpg'},
-      ...{type:'Movie'},
-      ...{tags:['Animated','Most Popular','Creative']}
-    });
-    this.contentListArr.push({
-      ...this.generateContent(
-        2,['Friends',`Friends is a 90's Comedy TV show`,'David Crane and Marta Kauffman']
-      ),
-      ...{type:'Sitcom'},
-      ...{imgURL:'https://cdn.wallpapersafari.com/83/31/b59cLE.jpg'}
-    });
-    this.contentListArr.push({
-      ...this.generateContent(
-        3,['Squid Game','Players in deep financial hardship, risk their lives to play a series of deadly Game','Hwang Dong-hyuk']
-      ),
-      ...{type:'Web-Series'}
-    });
-    this.contentListArr.push({
-      ...this.generateContent(
-        4,['3 Idiots',' Indian Hindi-language coming-of-age comedy-drama film written, edited and directed by Rajkumar Hirani','Rajkumar Hirani']
-      ),
-      ...{type:'Movie'},
-      ...{imgURL:'https://i.zoomtventertainment.com/story/Untitled_design_-_2020-05-01T135137.466.jpg'},
-      ...{tags:['Bollywood','Most Popular Hindispeaking Movie','Comedy','Based on book']}
-    });
-    this.contentListArr.push({
-      ...this.generateContent(
-        5,['A Beautiful Mind','A Beautiful Mind is a 2001 American biographical drama film directed by Ron Howard','Akiva Goldsman']
-      )
-    });
-    this.contentListArr.push({
-      ...this.generateContent(
-        6,['The Recruit','The Recruit is a 2003 American spy thriller film','Roger Donaldson']
-      )
-    });
-    this.contentListArr.push({
-      ...this.generateContent(
-        7,['The Shawshank Redemption','It is a 1994 American drama film','Frank Darabont']
-      )
-    });
-    this.contentListArr.push({
-      ...this.generateContent(
-        8,['The God Father','The Godfather is a 1972 American crime film','Francis Ford Coppola']
-      )
-    });
+  constructor(
+    private formBuilder: FormBuilder,
+    private entrnService: EntertainmentServiceService,
+    private msgService: MessageService
+  ){
+
+  }
+
+  ngOnInit() {
+    this.entrnService.getContent().subscribe((content)=>{
+      this.contentListArr = content;
+      this.msgService.add({status:1,msg:'Content array loaded!'});
+      this.onSubmit(4);
+      setTimeout(() => {
+        this.msgService.clear();
+      }, 2000);
+    })
   }
 }
